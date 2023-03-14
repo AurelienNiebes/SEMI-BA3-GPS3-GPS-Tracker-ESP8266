e9e024811bd83a6f6d9fc4fe37a85b2b45c7899c
AurelienNiebes <125119446+axellelambaere@users.noreply.github.com>
2023-03-14 10:21:35 +0100

    Test Include

---
+++ Codes/Test_Include/Test_Include.h
@@ -0,0 +1,7 @@
+#ifndef Test_Include_H
+  #define Test_Include_H
+  #include <Arduino.h> // Pour Serial et d'autres #include en fonction des besoins
+  
+ void maFonction_1();
+
+#endif
---
+++ Codes/Test_Include/Test_Include.ino
@@ -0,0 +1,9 @@
+#include "Test_Include.h"
+
+void setup() {
+  Serial.begin(115200);
+}
+ 
+void loop() {
+   maFonction_1();
+}
