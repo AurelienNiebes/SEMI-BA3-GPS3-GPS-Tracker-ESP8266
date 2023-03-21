5a0ee23be654978b40682e7261e1bde1efec409a
AurelienNiebes <100306790+AurelienNiebes@users.noreply.github.com>
2023-03-21 18:13:41 +0100

    Déplacé Lecture et Suppression SD
    Ces codes ne sont pas nécessaires pour le code principal

--- Codes/SD_LectureFichiers_OK/SD_LectureFichiers_OK.ino
+++ Codes/SD_LectureFichiers_OK/SD_LectureFichiers_OK.ino
@@ -3,20 +3,30 @@
 File myFile;
 String buffer;
 String filename = "GPS_data";
+
 const int chipSelect = 15;
+const int BaudRate =115200;
+
+void Serial_init(int BaudRate){
+  Serial.begin(BaudRate);
+  while (!Serial) {
+  }
+  Serial.println("Communication serie initialisee");
+}
+void SD_Init(int chipSelect) {
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+
+  Serial.println("initialization done.");
+}
 
 void setup() {
- 	Serial.begin(9600);
- 	while (!Serial) {
- 	}
- 	Serial.print("Initializing SD card...");
- 	if (!SD.begin(chipSelect)) {
- 			Serial.println("initialization failed!");
- 			return;
- 	}
-
- 	Serial.println("initialization done.");
-
+  Serial_init(BaudRate);
+  SD_Init(chipSelect);
+ 	
  	//Read file line by line
   
  	myFile = SD.open(filename + ".txt");
--- Codes/SD_SuppressionFichiers_OK/SD_SuppressionFichiers_OK.ino
+++ Codes/SD_SuppressionFichiers_OK/SD_SuppressionFichiers_OK.ino
@@ -3,25 +3,35 @@
 File myFile;
 String buffer;
 String filename = "GPS_data";
-const int chipSelect = 15;
 
-void setup() {
-   Serial.begin(9600);
+const int chipSelect = 15;
+const int BaudRate =115200;
+
+void Serial_init(int BaudRate){
+  Serial.begin(BaudRate);
   while (!Serial) {
   }
+  Serial.println("Communication serie initialisee");
+}
+
+void SD_Init(int chipSelect) {
   Serial.print("Initializing SD card...");
   if (!SD.begin(chipSelect)) {
-      Serial.println("initialization failed!");
-      return;
+    Serial.println("initialization failed!");
+    return;
   }
 
   Serial.println("initialization done.");
+}
 
-//Delete files
+void setup() {
+  Serial_init(BaudRate);
+  SD_Init(chipSelect);
+  //Delete files
 
- Serial.println("Removing " + filename + ".txt");
+  Serial.println("Removing " + filename + ".txt");
   SD.remove(filename + ".txt");
-  
+
   if (SD.exists(filename + ".txt")) {
     Serial.println(filename + ".txt exists.");
   } else {
@@ -29,4 +39,4 @@ void setup() {
   }
 }
 
-  void loop() { }
+void loop() {}
